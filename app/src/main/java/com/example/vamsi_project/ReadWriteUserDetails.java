761c931e44bc5dc3251e25d51476dff24afdc971
Vamsi Krishna Adapa <73695915+vamsiadapa1@users.noreply.github.com>
2023-04-14 23:17:41 -0500

    created the read wite user details page

---
+++ app/src/main/java/com/example/vamsi_project/ReadWriteUserDetails.java
@@ -0,0 +1,13 @@
+package com.example.vamsi_project;
+public class ReadWriteUserDetails {
+    public String name,age,mobile,gender;
+    public boolean admin;
+
+    public ReadWriteUserDetails(String name, String age, String mobile, String gender,boolean admin) {
+        this.name = name;
+        this.age = age;
+        this.mobile = mobile;
+        this.gender = gender;
+        this.admin=admin;
+    }
+}
